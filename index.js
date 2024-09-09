8797d0fafe717b5fd55b7bdf71b50191656575ea
Camilly Silveira <camillysillveira@gmail.com>
2024-09-09 17:42:50 -0300

    teste

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hello, World!")
+
+let numero1 = 7
+let numero2 = 6
+let numero3 = 8
+
+let soma = (numero1 + numero2 + numero3)
+let media = soma /3
+
+console.log("Resultado da média das notas" + media)
+
+
