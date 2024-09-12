fb1b8938f9b5d6fed3788ab68421c0d9b8ac9a2b
Camilly Silveira <camillysillveira@gmail.com>
2024-09-12 15:40:24 -0300

    exercicio6

---
+++ aula3/aula3ex6.js
@@ -0,0 +1,8 @@
+let petz = ["cachorro", "gato", "tartaruga"];
+
+const adicionarFimPetz = (pet) => {
+  console.log(petz.push("lagarto", "cobra")); // "lagarto", "cobra"
+  console.log(petz); // ["lagarto", "cobra"]
+};
+
+adicionarFimPetz();
