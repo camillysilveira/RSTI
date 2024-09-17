d2d3dc8089924b31583ee96c6aed013c84ca576f
Camilly Silveira <camillysillveira@gmail.com>
2024-09-17 14:48:42 -0300

    aulas

--- aula2/exercicio7.js
+++ aula2/exercicio7.js
@@ -1,10 +1,12 @@
 const readline = require('readline')
-const readline = readline.creatInterface({
+const rl = readline.createInterface({
   input: process.stdin, // recebe dados do usuário.
   output: process.stdout // log de texto e imprimir a string que é passada.
 })
 
-rl.question('Informe o seu saldo: ', (valor_reais) => { // rl. -> evitar erro e fechar a interface corretamente.
+rl.question('Informe o seu saldo: ', (valor_reais) => {
+  // rl. -> evitar erro e fechar a interface corretamente.
   let dolar = parseFloat(valor_reais) / 5.6 // parseFloat - analisa e retorna o resultado com n° decimal.
-  console.log(´Você receberá um total de: $${dolar} dolares.´)
+  console.log(`Você receberá um total de: $${dolar} dolares.`)
+  rl.close() // Fechar Interface
 })
