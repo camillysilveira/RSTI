c5aa0e0c91c716a90ea90cab238640fe2246599a
Camilly Silveira <camillysillveira@gmail.com>
2024-09-17 16:57:47 -0300

    aula

--- aula3/aula3ex8.js
+++ aula3/aula3ex8.js
@@ -1,8 +1,8 @@
 let petz = ['cachorro', 'gato', 'tartaruga', 'onça', 'tigre']
 
 const removePosicaoPetz = (pet) => {
-  petz.splice(3, 2, 'urubu')
+  petz.splice(3, 2, 'urubu') // pop() remove o último elemento - splice() remove de uma posição específica e substitui
   console.log(petz)
 }
 
-removePosicaoPetz()
+removePosicaoPetz('urubu')
