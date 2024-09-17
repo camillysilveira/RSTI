bd051ed581e91b9715854b8a9d3461dc705bdcb1
Camilly Silveira <camillysillveira@gmail.com>
2024-09-17 14:10:34 -0300

    aula

--- aula2/exercicio3.js
+++ aula2/exercicio3.js
@@ -1,13 +1,14 @@
 const readline = require('readline')
 const replace = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 })
 
-replace.question('Escreva seu nome: ', (nome)  => {
-    replace.question('Informe seu sálario: ', (salario) => {
-        console.log(`O funcionário ${nome} recebe o total de R$ ${salário} por mês.`)
-        replace.close()
-
-    })
+replace.question('Escreva seu nome: ', (nome) => {
+  replace.question('Informe seu sálario: ', (salario) => {
+    console.log(
+      `O funcionário ${nome} recebe o total de R$ ${salario} por mês.`
+    )
+    replace.close()
+  })
 })
--- aula3/aula3ex3.js
+++ aula3/aula3ex3.js
@@ -1,8 +1,8 @@
-let petz = ["cachorro", "gato", "tartaruga"]
+let petz = ['cachorro', 'gato', 'tartaruga']
 
 const adicionarInicioPetz = (pet) => {
-    petz.unshift("papagaio")
-    console.log(petz)
+  petz.unshift('papagaio')
+  console.log(petz)
 }
 
 adicionarInicioPetz()
