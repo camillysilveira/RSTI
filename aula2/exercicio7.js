473fb1100831473a99b18ea592d652bac7be9328
Camilly Silveira <camillysillveira@gmail.com>
2024-09-17 15:00:02 -0300

    aulas

--- aula2/exercicio7.js
+++ aula2/exercicio7.js
@@ -8,5 +8,5 @@ rl.question('Informe o seu saldo: ', (valor_reais) => {
   // rl. -> evitar erro e fechar a interface corretamente.
   let dolar = parseFloat(valor_reais) / 5.6 // parseFloat - analisa e retorna o resultado com n° decimal.
   console.log(`Você receberá um total de: $${dolar} dolares.`)
-  rl.close() // Fechar Interface
+  rl.close() // Fechar interface
 })
