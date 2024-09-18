fc093e7f21476b512d0e6bebbffe970afa5856c4
Camilly Silveira <camillysillveira@gmail.com>
2024-09-18 14:06:15 -0300

    aula

--- aula3/aula3ex9.js
+++ aula3/aula3ex9.js
@@ -1,9 +1,11 @@
 let petz = ['cachorro', 'gato', 'tartaruga', 'onça', 'tigre']
 
-const movePosicaoPetz = (pet) => {
-  petz.splice(4)
-  petz.push('tigre')
-  console.log(petz)
+const movePosicaoPetz = (array, index) => {
+  array.splice(index, 1) // index: É o índice do item que você deseja remover do array. Ao passar um número como argumento, a função vai utilizar esse número para identificar o item no array.
+  //Usar o Index permite que a função seja reutilizável
+  //permite também mover qualquer elemento apenas ultilizando o índice
+  //(index, 1) informa que ele será executado um única vez
+  console.log(array)
 }
 
-movePosicaoPetz()
+movePosicaoPetz(petz, 4)
