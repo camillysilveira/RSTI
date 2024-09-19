8a0b0b0b870ff311e326b51d3b375359ab8f5358
Camilly Silveira <camillysillveira@gmail.com>
2024-09-19 15:25:26 -0300

    aula7

---
+++ aula7/index.js
@@ -0,0 +1,95 @@
+const { select } = require('@inquire/prompts')
+
+const start = async () => {
+  while (true) {
+    const option = await select({
+      message: 'Menu > ',
+      choices: [
+        {
+          name: 'Cadastrar meta',
+          value: 'register'
+        },
+        {
+          name: 'Listar meta(s)',
+          value: 'list'
+        },
+        {
+          name: 'Sair',
+          value: 'out'
+        }
+      ]
+    })
+
+    switch (option) {
+      case 'registrer':
+        console.log('Vamos cadastrar uma meta!')
+        break
+      case 'list':
+        console.log('Vamos listar sua(s) meta(s)!')
+        break
+      case 'out':
+        console.log('Até a próxima!')
+        return
+    }
+  }
+}
+
+start()
+
+const { select, input } = require('@inquire/prompts')
+
+const goal = {
+  value: 'Tomar 3L de água por dia',
+  checked: false
+}
+const goals = [goal]
+
+const registerGoal = async () => {
+  const goal = await input({
+    message: 'Digite a sua meta: '
+  })
+
+  if (goal.length === 0) {
+    console.log('A meta não pode ser vazia!')
+    return
+  }
+
+  goals.push({ value: goal, checked: false })
+}
+
+const start = async () => {
+  while (true) {
+    const option = await select({
+      message: 'Menu > ',
+      choices: [
+        {
+          name: 'Cadastrar meta',
+          value: 'register'
+        },
+        {
+          name: 'Listar meta(s)',
+          value: 'list'
+        },
+        {
+          name: 'Sair',
+          value: 'out'
+        }
+      ]
+    })
+
+    switch (option) {
+      case 'register':
+        await registerGoal()
+        console.log(goals)
+        break
+      case 'list':
+        console.log('Vamos listar sua(s) meta(s)!')
+        break
+      case 'out':
+        console.log('Até a próxima!')
+        return
+    }
+  }
+}
+
+start()
