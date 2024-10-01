89bce16148add28e70e8d724066122609feb396c
Camilly Silveira <camillysillveira@gmail.com>
2024-10-01 13:41:45 -0300

    aula7

--- aula7/index.js
+++ aula7/index.js
@@ -1,7 +1,14 @@
-import fs from 'node:fs/promises'
-import { select, input, checkbox } from '@inquirer/prompts'
-
-const file = 'goals.json'
+import fs from 'node:fs/promises' // módulo "fs/promises" do Node.js é a biblioteca padrão para manipulação de sistema de arquivos.
+// /"promises" indica que estamos usando a versão prometida (promise-based) da API de arquivos, o que permite usar "async/await" para operações assíncronas
+//  "fs" permite realizar operações de maneira assíncrona e mais eficiente, sem bloquear o fluxo do programa.
+import { select, input, checkbox } from '@inquirer/prompts' // importando as três funções da biblioteca "@inquirer/prompts".
+// amplamente usada em CLI (Command Line Interface) para criar menus interativos e capturar entradas do usuário diretamente no terminal.
+/* 
+--> select: Exibe um menu de seleção para o usuário escolher uma opção entre várias.
+--> input: Permite que o usuário insira um texto (como registrar o nome de uma meta, por exemplo).
+--> checkbox: Apresenta uma lista de opções para o usuário marcar várias delas.
+*/
+const file = 'goals.json' // arquivo onde os dados das metas provavelmente serão armazenados e lidos pelo sistema.
 
 let goals
 
@@ -108,17 +115,22 @@ const listIncompletedGoals = async () => {
 }*/
 
 const porcentualCompletedGoals = async () => {
-  const completedGoals = goals.filter((goal) => goal.checked)
+  // async: Permite o uso de operações assíncronas.
+  const completedGoals = goals.filter((goal) => goal.checked) // nova variável completedGoals: metas marcadas como conclúidas.
+  // filter: cria um novo array, incluindo apenas o que atende a determinada condição.
+  // checked neste caso será true, já que é necessário que a meta seja marcada como concluída.
+  const parcial = `${completedGoals.length}/${goals.length}` // cria uma variável denominada parcial, sendo uma string no formato x/y.
+  // completedGoals: Retorna o n° de metas concluídas
+  // goals.length: Retorna o n° total de metas cadastradas.
 
-  const parcial = `${completedGoals.length}/${goals.length}`
+  const percentage = (completedGoals.length / goals.length) * 100 // Divide o n° de metas concluídas pelo n° de metas cadastradas
+  // formando uma fração que será multiplicada por 100, retornando um valor porcentual de metas.
 
-  const percentage = (completedGoals.length / goals.length) * 100
-
-  console.log(`Você completou ${percentage.toFixed(2)}% das metas.`)
+  console.log(`Você completou ${percentage.toFixed(2)}% das metas.`) // toFixed(2): arredonda o valor da porcentagem para duas casas decimais
   console.log(`Você completou ${completedGoals.length}/${goals.length} metas.`)
 
   return {
-    parcial: `${completedGoals.length}/${goals.length}`,
+    parcial: `${completedGoals.length}/${goals.length}`, // Vai retornar o objeto com as duas propriedades.
     porcentagem: percentage.toFixed(2)
   }
 }
@@ -210,15 +222,18 @@ const showMessage = () => {
 }
 
 const start = async () => {
-  await fetchGoals()
-
+  await fetchGoals() // fetchGoals(): função assíncrona, responsável por buscar metas existentes da fonte de dados, no caso "goals.json".
+  // O uso do "await" faz com que o código espere até que todas as metas sejam carregadas antes de prosseguir.
   while (true) {
-    showMessage()
-    await saveGoals()
-
+    // cria um "loop infinito" até que o programa seja interrompido manualmente "sair/out" ou "break".
+    // --> o programa pode ficar aguardando o usuário escolher opções repetidamente, e o loop só seria interrompido quando o usuário sair ou o programa for encerrado.
+    showMessage() // Mostra uma mensagem ou Menu.
+    await saveGoals() // Chama a função assíncrona responsável por salvar as metas.
     const option = await select({
-      message: 'Menu > ',
+      // Cria um menu interativo, sendo uma função assíncrona utilizando "await" para esperar o usuário escolher a opção desejada.
+      message: 'Menu > ', // Mostra a mensagem que será exibida para o usuário, neste caso o "Menu".
       choices: [
+        // Opções disponíveis no menu.
         {
           name: 'Cadastrar meta',
           value: 'register'
@@ -232,8 +247,8 @@ const start = async () => {
           value: 'completed'
         },
         {
-          name: 'Listar meta(s) conclúidas(s) (detalhado)',
-          value: 'porcentualAlternative'
+          name: 'Listar meta(s) conclúidas(s) (detalhado)', // Nome do texto sendo exibido.
+          value: 'porcentualAlternative' // Valor que será retornado pela função.
         },
         {
           name: 'Listar meta(s) não realizada(s)',
@@ -254,11 +269,14 @@ const start = async () => {
       ]
     })
 
-    switch (option) {
-      case 'register':
-        await registerGoal()
-        console.log(goals)
-        break
+    switch (
+      option // Depois que o usuário escolher a opção do menu, seu valor será armazenado na variável "option"
+    ) {
+      // switch é ultilizado para decidir o que fazer com o valor recibido na escolha do usuário.
+      case 'register': // se o valor de "option" for "register" será chamado a função "registerGoal()".
+        await registerGoal() // Permitindo que o usuário adicione uma nova meta.
+        console.log(goals) // Depois vai exibir todas as metas na variável "goals"
+        break // Interrompe a execução do switch em cada caso, evitando que fique verificando os outros "case" após executar o atual.
       case 'list':
         await listGoals()
         break
@@ -268,8 +286,8 @@ const start = async () => {
       case 'incompleted':
         await listIncompletedGoals()
         break
-      case 'porcentualAlternative':
-        await porcentualCompletedGoals()
+      case 'porcentualAlternative': // option: 'porcentualAlternative'.
+        await porcentualCompletedGoals() // chamando a função "porcentualCompletedGoals()" definida anteriormente.
         break
       case 'update':
         await updateGoals()
