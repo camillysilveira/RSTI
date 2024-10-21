daf6a56fc2261a6be0d6f55e1dd787adc2436cfc
Camilly Silveira <camillysillveira@gmail.com>
2024-10-21 17:09:17 -0300

    login form id

--- FrontEnd/login-form/main.js
+++ FrontEnd/login-form/main.js
@@ -1,4 +1,15 @@
-const form = document.getElementById('loginForm')
+function togglePassword() {
+  document
+    .querySelectorAll('.eye')
+    .forEach((eye) => eye.classList.toggle('hide'))
+
+  const password = document.getElementById('password')
+  const type =
+    password.getAttribute('type') === 'password' ? 'text' : 'password'
+  password.setAttribute('type', type)
+}
+
+/*const form = document.getElementById('loginForm')
 
 form.addEventListener('submit', function (event) {
   event.preventDefault()
@@ -34,17 +45,17 @@ const loggedUser = localStorage.getItem('login-form')
 if (valid) {
   alert('Formulário enviado com sucesso!')
   localStorage.setItem('login-form', 'loggedUser')
-}
+}*/
 
 /*   if (valid) {
     alert("Formulário enviado com sucesso!");
     localStorage.setItem("login-form", "loggedUser");
-  } */
+  }
 
 function validatedEmail(email) {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
   return regex.test(email)
-}
+}*/
 
 /* function tooglePassword() {
   document
@@ -60,15 +71,15 @@ function validatedEmail(email) {
 }
  */
 
-document
-  .getElementById('loginForm')
-  .addEventListener('submit', async function (event) {
+/*document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (event) {
     event.preventDefault()
 
-    const username = document.getElementById('name').value
-    const password = document.getElementById('password').value
+    const username = document.getElementById("name").value
+    const password = document.getElementById("password").value
 
-    const responseElement = document.getElementById('response')
+    const responseElement = document.getElementById("response")
     responseElement.innerHTML = ''
 
     try {
@@ -90,4 +101,60 @@ document
     } catch (error) {
       responseElement.innerHTML = `<p>Erro na requisição: ${error.message}</p>`
     }
+  })*/
+
+/*const validUserMail = "usuario@mail.com";
+const validPassword = "senha123";*/
+
+// Verificar se existe um token salvo em LocalStorage
+document.addEventListener('DOMContentLoaded', function () {
+  const storedToken = localStorage.getItem('accessToken')
+
+  console.log(storedToken)
+  if (storedToken) {
+    window.location.href = './pages/loggedUser/index.html'
+    return
+  }
+})
+
+document
+  .getElementById('login-form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const userName = document.getElementById('email').value
+    const userPassword = document.getElementById('password').value
+
+    fetch('https://dummyjson.com/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: userName,
+        password: userPassword,
+        expiresInMins: 30
+      })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && data.accessToken) {
+          console.log('Login bem-sucedido!')
+
+          localStorage.setItem('accessToken', data.accessToken)
+          window.location.href = './pages/loggedUser/index.html'
+        } else {
+          console.log('Erro no login: ', data.message)
+        }
+      })
+      .catch((error) => {
+        console.log('Erro na requisição: ', error)
+      })
   })
+
+/* if (userMail === validUserMail && userPassword === validPassword) {
+    console.log("Login bem-sucedido");
+
+    localStorage.setItem(validUserMail, uuid);
+  } else {
+    console.log("Erro");
+  }
+});*/
